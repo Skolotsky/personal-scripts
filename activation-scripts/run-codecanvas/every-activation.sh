e316efda3a1cb4e2161930a07749046553c7f33e
Abasov Akif <akiff@yandex.ru>
2025-03-07 10:50:06 +0100

    Update every-activation.sh

--- activation-scripts/run-codecanvas/every-activation.sh
+++ activation-scripts/run-codecanvas/every-activation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+
+echo "Run Sandbox"
+canvas run-configuration codecanvas "Run Sandbox"
 echo "Run CodeCanvas"
 canvas run-configuration codecanvas "Run CodeCanvas"
+echo "Wait for Sandbox to open"
+until curl -s --head --fail $URL | grep -q "200 OK"; do
+  sleep 1
+done
+echo "Open Sandbox"
+canvas run-configuration codecanvas "Open Sandbox"
