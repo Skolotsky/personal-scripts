49760114bf37c81d2fbb98cc3b49658d2ebefcd2
Abasov Akif <akiff@yandex.ru>
2025-04-02 15:59:46 +0000

    Fix

--- activation-scripts/run-codecanvas/open-cursor.sh
+++ activation-scripts/run-codecanvas/open-cursor.sh
@@ -2,5 +2,8 @@
 
 $REPO_PATH/activation-scripts/run-codecanvas/run-vscode-and-cursor.sh
 
+echo "Wait 10 seconds to ensure that Thin Client is and can open the Cursor"
+sleep 10
+
 echo "Open Cursor"
 canvas run-configuration codecanvas "Cursor"
