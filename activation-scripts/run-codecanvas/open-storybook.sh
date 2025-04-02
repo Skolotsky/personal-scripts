9dfe3bc269f3b418b5d18a34acb1abeb6aff85c2
Abasov Akif <akiff@yandex.ru>
2025-04-02 15:34:00 +0000

    fix

--- activation-scripts/run-codecanvas/open-storybook.sh
+++ activation-scripts/run-codecanvas/open-storybook.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Run Stroybook"
-canvas run-configuration codecanvas "Run Stroybook"
+echo "Run Storybook"
+canvas run-configuration codecanvas "Run Storybook"
 
 echo "Wait for Storybook to be ready"
 $REPO_PATH/scripts/waitFor.sh "http://localhost:6006"
 
 echo "Open Storybook"
-canvas run-configuration codecanvas "Open Stroybook"
+canvas run-configuration codecanvas "Open Storybook"
