3951204e1396820d77d867b97d9a0abebb08bf15
Abasov Akif <akiff@yandex.ru>
2025-04-08 07:35:36 +0000

    Fix

--- activation-scripts/run-codecanvas/every-activation.sh
+++ activation-scripts/run-codecanvas/every-activation.sh
@@ -6,6 +6,9 @@ export REPO_PATH=$PWD
 
 cd /mnt/jetbrains/work/codecanvas
 
+# avoid races with ide startup
+sleep 5
+
 $REPO_PATH/activation-scripts/run-codecanvas/open-sandbox.sh &
 $REPO_PATH/activation-scripts/run-codecanvas/open-storybook.sh &
 $REPO_PATH/activation-scripts/run-codecanvas/open-cursor.sh &
